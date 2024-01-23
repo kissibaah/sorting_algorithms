d8818288ef4e9e2fabc62fe1cd2114c0b8da8525
kissibaah <paaquesi143@gmail.com>
2024-01-23 08:40:20 -0800

    Sorting Algorithm Project

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * swap_ints - this Swaps two integers in the array
+ * @x: The first integer to be swapped
+ * @y: The second integer to be swappped.
+ */
+void swap_ints(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * bubble_sort - for sorting an array of integers
+ * in ascending order.
+ * @array: array of integers to be sorted.
+ * @size: size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
+
